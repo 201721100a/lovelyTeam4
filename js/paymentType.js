b2e4adfb39addbf1b014987a7dd080d12dae30b6
HC <wkqkrla12@gmail.comgit config --global user.name HC git config --global user.email wkqkrla12@gmail.com git config --global user.email wkqkrla12@gmail.com>
2023-04-12 20:46:16 +0900

    paymentType 수정

--- js/paymentType.js
+++ js/paymentType.js
@@ -1,8 +1,13 @@
+// import list from "./list.js";
+
 export default class PaymentType {
   #canvas;
   #ctx;
   #cancelImg;
   #typeImg;
+  #typeImg1;
+  #typeImg2;
+
   constructor() {
     this.#canvas = document.createElement("canvas");
     this.#ctx = this.#canvas.getContext("2d");
@@ -15,6 +20,8 @@ export default class PaymentType {
     this.#canvas.height = 700;
     this.#cancelImg = document.getElementById("paymentType");
     this.#typeImg = document.getElementById("paymentTypeCancel");
+    this.#typeImg1 = document.getElementById("cardType");
+    this.#typeImg2 = document.getElementById("QRType");
     this.#canvas.onclick = this.clickHandler.bind(this);
     this.draw();
   }
@@ -22,11 +29,31 @@ export default class PaymentType {
   clickHandler(e) {
     let x = e.offsetX;
     let y = e.offsetY;
+    console.log(`x: ${x}, y:${y}`);
 
     if (520 <= x && 163 <= y && x <= 564 && y <= 193) {
+      //취소 좌표
       document.body.removeChild(this.#canvas);
     }
+
+    if (100 <= x && 300 <= y && x <= 260 && y <= 465) {
+      this.#ctx.fillStyle = "white";
+      this.#ctx.fillRect(10, 150, 580, 380);
+      this.#ctx.drawImage(this.#typeImg1, 100, 240, 380, 250);
+    }
+
+    if (310 <= x && 300 <= y && x <= 460 && y <= 470) {
+      this.#ctx.fillStyle = "white";
+      this.#ctx.fillRect(10, 150, 580, 380);
+      this.#ctx.drawImage(this.#typeImg2, 100, 240, 380, 250);
+    }
+
+    // this.#ctx.clearRect(300, 300, 170, 170);
     // 미션완료 추가
+    setTimeout(() => {
+      this.#ctx.fillStyle = "white";
+      this.#ctx.fillRect(10, 150, 580, 380);
+    }, 3000);
   }
 
   draw() {
